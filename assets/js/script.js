edf149f68cb98038d85f22629fff7e307f85d862
aorich <rich.aidan@gmail.com>
2021-09-19 08:37:00 -0400

    added api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 console.log("wassup");
+
+var APIKey = "2d62885b9291cabc94e793d6b1fc4f27"
+
+var city;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+// Now I need a fetch(queryURL)
+
